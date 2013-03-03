2787c2254100ea0ccf5b8331753f3c4dafc7ccc0
OpenShift guest <teran4work@gmail.com>
2013-03-03 18:46:56 -0500

    Hello NodeJs

---
+++ server.js
@@ -0,0 +1,10 @@
+var HOST = process.env.IP;
+var PORT = process.env.PORT;
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(PORT);
